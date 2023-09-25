1e0b4c07e2f55f519cbe95a9360ad86102f6b972
lufione <luffitak@hotmail.com>
2023-09-25 14:56:56 +0700

    nodejs file

---
+++ appConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: "Interior Design Photos",
+  tagline: "Contents Article & Image",
+  description: "More interior design ideas photos with new inovation and new design from best and famous designer",
+  keywords: "interior, exterior, home design, furniture,b athroom, living room, bathroom",
+  author: "Fathur",
+  favicon: "https://pugjs.org/images/favicon.ico",
+  imageCount: 30,
+  removeBadWords: "porn,nude,sex,sexiest,topless,vagina,penis",
+};
